eca75bccf3e71206ad76b9a381f49322494212ed
thedeukalion <jimmy.wiberg@raceone.com>
2020-09-16 13:49:36 +0200

    Android X fix

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -37,7 +37,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.util.Base64;
 
 import org.apache.cordova.BuildHelper;
